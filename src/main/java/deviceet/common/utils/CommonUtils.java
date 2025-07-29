e9c55c83771ba63fa35c12292e75cd57952cf30a
Teng Yun <davenkin@126.com>
2025-07-29 20:20:25 +0800

    update

--- src/main/java/deviceet/common/utils/CommonUtils.java
+++ src/main/java/deviceet/common/utils/CommonUtils.java
@@ -1,13 +1,15 @@
 package deviceet.common.utils;
 
+import deviceet.common.exception.ServiceException;
+
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
+import static deviceet.common.exception.ErrorCode.SYSTEM_ERROR;
+import static java.util.Objects.requireNonNull;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
-//todo:split
 public class CommonUtils {
-    private static final String SPRING_CGLIB_CLASS_SEPARATOR = "$$";
 
     public static String requireNonBlank(String str, String message) {
         if (isBlank(str)) {
@@ -17,28 +19,32 @@ public class CommonUtils {
     }
 
     public static Class<?> singleParameterizedArgumentClassOf(Class<?> aClass) {
-        // The aClass might be proxied by Spring CGlib, so we need to get the real targeted class
-        Class<?> realClass = aClass.getName().contains(SPRING_CGLIB_CLASS_SEPARATOR) ? aClass.getSuperclass() : aClass;
+        Type genericSuperclass = aClass.getGenericSuperclass();
+        if (genericSuperclass instanceof ParameterizedType theSuperclass) {
+            Type[] typeArguments = theSuperclass.getActualTypeArguments();
+            if (typeArguments.length != 1) {
+                throw new ServiceException(SYSTEM_ERROR,
+                        "Expecting exactly one parameterized type argument for[" + aClass.getName() + "],but got " + typeArguments.length + ". ");
+            }
 
-        Type genericSuperclass = realClass.getGenericSuperclass();
-        if (!(genericSuperclass instanceof ParameterizedType)) {
-            return null;
+            Type actualTypeArgument = typeArguments[0];
+            if (actualTypeArgument instanceof Class<?>) {
+                return (Class<?>) actualTypeArgument;
+            } else {
+                throw new ServiceException(SYSTEM_ERROR,
+                        "The argument type[" + actualTypeArgument.getTypeName() + "] is not of Class type for: " + aClass.getName());
+            }
+        } else {
+            throw new ServiceException(SYSTEM_ERROR,
+                    "The super class of [" + aClass.getName() + "] is not of parameterized type.");
         }
-
-        Type[] actualTypeArguments = ((ParameterizedType) genericSuperclass).getActualTypeArguments();
-
-        if (actualTypeArguments.length != 1) {
-            throw new RuntimeException("Expecting exactly one parameterized type argument for " + realClass);
-        }
-
-        Type actualTypeArgument = actualTypeArguments[0];
-        if (actualTypeArgument instanceof Class) {
-            return (Class<?>) actualTypeArgument;
-        }
-        return null;
     }
 
     public static String mongoConcatFields(String... fields) {
+        requireNonNull(fields, "Fields cannot be null.");
+        for (String arg : fields) {
+            requireNonBlank(arg, "Element of fields is blank.");
+        }
         return String.join(".", fields);
     }
 
