d15a6a21887861e2e3916690dedd380ca3139eaa
Teng Yun <davenkin@126.com>
2025-07-30 23:31:23 +0800

    update

--- src/main/java/deviceet/common/security/Principal.java
+++ src/main/java/deviceet/common/security/Principal.java
@@ -8,38 +8,27 @@ import java.util.Set;
 import static deviceet.common.exception.ServiceException.accessDeniedException;
 import static deviceet.common.security.Role.ROOT;
 import static deviceet.common.utils.CommonUtils.requireNonBlank;
-import static deviceet.common.utils.Constants.PLATFORM_TENANT_ID;
 import static java.util.Objects.requireNonNull;
 
 @Getter
 @EqualsAndHashCode
 public class Principal {
-    private final String tenantId;
     private final String userId;
     private final String userName;
     private final Set<Role> roles;
+    private final String orgId;
 
-    private Principal(String tenantId, String userId, String userName, Role role) {
-        requireNonBlank(tenantId, "tenantId must not be blank.");
+    public Principal(String userId, String userName, Role role, String orgId) {
+        requireNonBlank(orgId, "orgId must not be blank.");
         requireNonBlank(userId, "userId must not be blank.");
         requireNonBlank(userName, "userName must not be blank.");
         requireNonNull(role, "role must not be null.");
 
-        this.tenantId = tenantId;
+        this.orgId = orgId;
         this.userId = userId;
         this.userName = userName;
         this.roles = role.effectiveRoles();
     }
-
-    public static Principal rootPrincipal(String userId, String userName) {
-        return new Principal(PLATFORM_TENANT_ID, userId, userName, ROOT);
-    }
-
-    public static Principal tenantPrincipal(String tenantId, String userId, String userName, Role role) {
-        return new Principal(tenantId, userId, userName, role);
-    }
-
-    //todo: add tenantPrincipalFromUser(User user)
 
     public boolean isRoot() {
         return this.roles.contains(ROOT);
@@ -55,4 +44,12 @@ public class Principal {
         }
     }
 
+    @Override
+    public String toString() {
+        return "Principal{" +
+               "userId='" + userId + '\'' +
+               ", userName='" + userName + '\'' +
+               ", orgId='" + orgId + '\'' +
+               '}';
+    }
 }
