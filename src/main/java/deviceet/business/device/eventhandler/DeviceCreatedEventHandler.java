db9b2554928bd1f524dacc7865fd282074e2a9c3
Teng Yun <davenkin@126.com>
2025-08-04 23:29:02 +0800

    update

--- src/main/java/deviceet/business/device/eventhandler/DeviceCreatedEventHandler.java
+++ src/main/java/deviceet/business/device/eventhandler/DeviceCreatedEventHandler.java
@@ -1,6 +1,5 @@
 package deviceet.business.device.eventhandler;
 
-import deviceet.business.device.domain.DeviceRepository;
 import deviceet.business.device.domain.event.DeviceCreatedEvent;
 import deviceet.common.event.consume.AbstractEventHandler;
 import deviceet.common.notification.NotificationService;
@@ -13,12 +12,9 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class DeviceCreatedEventHandler extends AbstractEventHandler<DeviceCreatedEvent> {
     private final NotificationService notificationService;
-    private final DeviceRepository deviceRepository;
 
     @Override
     public void handle(DeviceCreatedEvent event) {
-        deviceRepository.cachedOrgDevices(event.getArOrgId());
-        deviceRepository.cachedOrgDevices(event.getArOrgId());
         notificationService.notifyOnDeviceCreated(event.getArId());
     }
 }
