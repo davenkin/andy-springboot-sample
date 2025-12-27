e176c20a93b6cfd43ce869d0efb71f36211310f5
Teng Yun <davenkin@126.com>
2025-12-27 18:14:59 +0800

    update

--- src/test/java/com/company/andy/IntegrationTest.java
+++ src/test/java/com/company/andy/IntegrationTest.java
@@ -5,8 +5,6 @@ import com.company.andy.common.event.DomainEventType;
 import com.company.andy.common.event.consume.EventConsumer;
 import com.company.andy.common.event.publish.PublishingDomainEvent;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
@@ -14,9 +12,6 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
-import redis.embedded.RedisServer;
-
-import java.io.IOException;
 
 import static com.company.andy.common.util.CommonUtils.mongoConcatFields;
 import static com.company.andy.common.util.CommonUtils.requireNonBlank;
@@ -32,8 +27,6 @@ import static org.springframework.data.mongodb.core.query.Query.query;
 //@ActiveProfiles("it-local")
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public abstract class IntegrationTest {
-    private static RedisServer redisServer;
-
     @Autowired
     protected MongoTemplate mongoTemplate;
 
@@ -45,22 +38,6 @@ public abstract class IntegrationTest {
 
     @Autowired
     protected Environment environment;
-
-    @BeforeAll
-    public static void beforeAll() {
-        try {
-            // RedisServer is only used by application-it.yaml, but not application-it-local.yaml as it uses real Redis server from local machine
-            redisServer = new RedisServer(6124);
-            redisServer.start();
-        } catch (IOException e) {
-            log.error("Failed to start embedded Redis server.", e);
-        }
-    }
-
-    @AfterAll
-    public static void afterAll() throws IOException {
-        redisServer.stop();
-    }
 
     protected <T extends DomainEvent> T latestEventFor(String arId, DomainEventType type, Class<T> eventClass) {
         requireNonBlank(arId, "arId must not be blank.");
